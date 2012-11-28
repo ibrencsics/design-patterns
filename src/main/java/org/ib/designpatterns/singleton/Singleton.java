ba13bc02edf08abbc8eee190d16fdb9b5e3e85db
Ivan Brencsics <ivan.brencsics@gmail.com>
2012-11-28 22:43:11 +0100

    src

---
+++ src/main/java/org/ib/designpatterns/singleton/Singleton.java
@@ -0,0 +1,11 @@
+package org.ib.designpatterns.singleton;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ibrencsics
+ * Date: 2012.11.28.
+ * Time: 22:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Singleton {
+}
